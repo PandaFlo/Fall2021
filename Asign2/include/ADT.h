3b92f6acf65b0a9a7759c0733e3f0e66f2f441cb
PandaFlo <96489534+PandaFlo@users.noreply.github.com>
2021-12-21 11:26:29 -0500

    Add files via upload

---
+++ Asign2/include/ADT.h
@@ -0,0 +1,30 @@
+/*
+ * ADT.h
+ *
+ *  Created on: Oct 31, 2021
+ *      Author: Flash
+ */
+#include <string>
+#include "BST.h"
+#include "person.h"
+
+// Class for the Abstract Data Type
+struct ADT {
+public:
+    // Helpers
+    enum ListMode {PREORDER, INORDER, POSTORDER};
+    enum SearchMode {NAME, DOB};
+    enum DOBElement {NONE, MONTH, DAY, YEAR};
+
+    // The tree that the ADT is going to be the "front-end" for
+    BST<Person> *tree;
+
+    // Method Definitions
+    ADT();
+    void List(ListMode m);
+    bool Find(Person *person);
+    void Search(SearchMode m, std::string search = "", DOBElement dobElement = NONE, int query = -1);
+    void Add(Person *person);
+    void Remove(Person person);
+    void Update(Person *person, Person *updated);
+};
